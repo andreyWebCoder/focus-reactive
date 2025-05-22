e1b82811e0e67c886a4d8875310095fdbe0add19
“Andrey <foresteraws@gmail.com>
2025-05-22 11:43:43 +0300

    Fix gulp path

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -8,38 +8,39 @@ const srcFolder = `./src`;
 
 // Пути к папкам и файлам проекта
 export const path = {
-    build: {
-        html: `${buildFolder}/`,
-        js: `${buildFolder}/js/`,
-        css: `${buildFolder}/css/`,
-        images: `${buildFolder}/img/`,
-        fonts: `${buildFolder}/fonts/`,
-        files: `${buildFolder}/files/`
-    },
-    src: {
-        // html: `${srcFolder}/*.html`,
-        html: `${srcFolder}/*.njk`,
-        // pug: `${srcFolder}/*.pug`,
-        js: [`${srcFolder}/js/app.js`, `${srcFolder}/js/vendors.js`],
-        scss: [`${srcFolder}/scss/style.scss`, `${srcFolder}/scss/vendors.scss`],
-        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
-        svg: `${srcFolder}/img/**/*.svg`,
-        // fonts: `${srcFolder}/fonts/*.*`,
-        files: `${srcFolder}/files/**/*.*`,
-        // svgicons: `${srcFolder}/svgicons/*.svg`,
-    },
-    watch: {
-        js: `${srcFolder}/js/**/*.js`,
-        scss: `${srcFolder}/scss/**/*.scss`,
-        // html: `${srcFolder}/**/*.html`,
-        html: `${srcFolder}/**/*.njk`,
-        // html: `${srcFolder}/**/*.pug`,
-        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
-        files: `${srcFolder}/files/**/*.*`,
-    },
-    clean: buildFolder,
-    buildFolder: buildFolder,
-    rootFolder: rootFolder,
-    srcFolder: srcFolder,
-    ftp: `` // Путь к нужной папке на удаленном сервере. gulp добавит имя папки проекта автоматически
+	build: {
+		html: `${buildFolder}/`,
+		js: `${buildFolder}/js/`,
+		css: `${buildFolder}/css/`,
+		images: `${buildFolder}/img/`,
+		fonts: `${buildFolder}/fonts/`,
+		files: `${buildFolder}/files/`
+	},
+	src: {
+		// html: `${srcFolder}/*.html`,
+		html: `${srcFolder}/*.njk`,
+		// pug: `${srcFolder}/*.pug`,
+		js: [`${srcFolder}/js/app.js`, `${srcFolder}/js/vendors.js`],
+		scss: `${srcFolder}/scss/style.scss`,
+		// scss: [`${srcFolder}/scss/style.scss`, `${srcFolder}/scss/vendors.scss`],
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+		svg: `${srcFolder}/img/**/*.svg`,
+		// fonts: `${srcFolder}/fonts/*.*`,
+		files: `${srcFolder}/files/**/*.*`,
+		// svgicons: `${srcFolder}/svgicons/*.svg`,
+	},
+	watch: {
+		js: `${srcFolder}/js/**/*.js`,
+		scss: `${srcFolder}/scss/**/*.scss`,
+		// html: `${srcFolder}/**/*.html`,
+		html: `${srcFolder}/**/*.njk`,
+		// html: `${srcFolder}/**/*.pug`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+		files: `${srcFolder}/files/**/*.*`,
+	},
+	clean: buildFolder,
+	buildFolder: buildFolder,
+	rootFolder: rootFolder,
+	srcFolder: srcFolder,
+	ftp: `` // Путь к нужной папке на удаленном сервере. gulp добавит имя папки проекта автоматически
 };
